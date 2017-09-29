e850abbc2ab39706963b7e1a58f0229dba6ad87d
jiachao <jiaccool@163.com>
2017-09-29 17:48:13 +0800

    no message

---
+++ src/model/billof-work-main.ts
@@ -0,0 +1,14 @@
+/**
+ * 工程量清单主信息
+ * Created by jiachao on 2017-09-29.
+ */
+export class BillOfWorkMain{
+
+	payCode :string;//付款单号
+	
+	sequence:string;//序号
+	
+	listNumber:string;//清单项目编号
+
+	listName :string;//清单项目名称
+}
--- src/pages/query-condition/query-condition.ts
+++ src/pages/query-condition/query-condition.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Utils } from '../../providers/Utils';
+
 
 /**
  * Generated class for the QueryConditionPage page.
@@ -15,11 +17,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class QueryConditionPage {
 
+  startDate:string;
+  endDate:string;
+  relationship:string;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad QueryConditionPage');
+    this.relationship="1";
+    this.startDate=Utils.dateFormat(new Date());
+    this.endDate=(new Date()).toISOString();
+    console.log(this.endDate);
   }
 
   openQuery(){
